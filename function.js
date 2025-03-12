ab39314624a8a62bb8c0b770a4a7d9edeea37773
Volodymyr Zhovtobriukh <eonwk7@gmail.com>
2025-03-12 14:31:00 +0200

    Function

---
+++ function.js
@@ -0,0 +1,35 @@
+// Задача 1.1
+/*'use strict'
+function checkAge(age){
+   return ( age>18) ? true : confirm("Родители разрешили?");  
+}
+*/
+
+// задача 1.2
+
+/* function checkAge(age){
+ return (age >18) || confirm("Родители разрешили?");
+}
+ */
+
+// 2
+/*
+function min(a,b){
+    return (a<b) ? a:b;
+}
+
+alert(min(1,3));
+*/
+
+// 3
+"use strict"
+function pow(x,n){
+    let result = x**n;
+    return result;
+}
+let x = prompt('Введите значение x');
+let n = prompt('Введите значение y');
+
+if (n >=1 && n%1 == 0){
+alert(pow(x,n));
+} else alert("Введи нормальное число");

37c79bb990ca3b9f0c72b6920067587cb62d041b
Volodymyr Zhovtobriukh <eonwk7@gmail.com>
2025-05-06 00:42:45 +0300

    Try to understnd obj

---
+++ obj.js
@@ -0,0 +1,37 @@
+// Задача 1
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+
+// alert( user.name);
+
+// delete user.name;
+/*
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete:130
+}
+
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+alert(sum);
+*/
+
+// let obj = {
+//     car: 21,
+//     houses: 1,
+//     trees: "some plant",
+//     branches: 3,
+// }
+
+// function multiplyNumeric(obj) {
+//     for (let key in obj  ) {
+//         if (typeof obj[key] === 'number') {
+//             obj.[key]*2
+            
+//         } 
+//     }
+// }
+
+
